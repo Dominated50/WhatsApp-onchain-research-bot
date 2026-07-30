dbfe2ef14e51487117520e39be591e71e63a5b2c
Dominated50 <161648046+Dominated50@users.noreply.github.com>
2026-07-30 07:50:01 +0100

    Implement getHoldersUrl for chain explorers
    Added getHoldersUrl function to retrieve holders URLs for various chains.

--- services/chains.js
+++ services/chains.js
@@ -22,4 +22,21 @@ function detectAddressType(address) {
   return "unknown";
 }
 
-module.exports = { DEXSCREENER_TO_GOPLUS, isSolanaChain, detectAddressType };
+const EXPLORER_HOLDERS_URL = {
+  ethereum: (addr) => `https://etherscan.io/token/${addr}#balances`,
+  bsc: (addr) => `https://bscscan.com/token/${addr}#balances`,
+  polygon: (addr) => `https://polygonscan.com/token/${addr}#balances`,
+  arbitrum: (addr) => `https://arbiscan.io/token/${addr}#balances`,
+  optimism: (addr) => `https://optimistic.etherscan.io/token/${addr}#balances`,
+  base: (addr) => `https://basescan.org/token/${addr}#balances`,
+  avalanche: (addr) => `https://snowtrace.io/token/${addr}#balances`,
+  fantom: (addr) => `https://ftmscan.com/token/${addr}#balances`,
+  solana: (addr) => `https://solscan.io/token/${addr}#holders`,
+};
+
+function getHoldersUrl(dexChainId, address) {
+  const builder = EXPLORER_HOLDERS_URL[dexChainId];
+  return builder ? builder(address) : null;
+}
+
+module.exports = { DEXSCREENER_TO_GOPLUS, isSolanaChain, detectAddressType, getHoldersUrl };
