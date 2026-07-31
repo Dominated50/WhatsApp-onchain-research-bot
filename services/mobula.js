ac16f886f2dc2af2dca6e387eb4d3dc246c91a35
Dominated50 <161648046+Dominated50@users.noreply.github.com>
2026-07-31 21:52:22 +0100

    Update BSC name in CHAIN_TO_MOBULA

--- services/mobula.js
+++ services/mobula.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const CHAIN_TO_MOBULA = {
   ethereum: "ethereum",
-  bsc: "bnb smart chain",
+  bsc: "bnb",
   polygon: "polygon",
   arbitrum: "arbitrum",
   optimism: "optimism",
